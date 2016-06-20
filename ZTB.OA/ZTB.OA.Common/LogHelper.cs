619aa8a193aa09f502ef6ef1be892e3c2cf55887
zhenglong2015 <853222835@qq.com>
2016-06-20 21:23:04 +0800

    .

---
+++ ZTB.OA/ZTB.OA.Common/LogHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ZTB.OA.Common
+{
+    public class LogHelper
+    {
+        public static Queue<string> ExceptionStringQueue = new Queue<string>();
+        public static List<ILogWrite> logWriteList = new List<ILogWrite>();
+
+        static LogHelper()
+        {
+            logWriteList.Add(new TextWrite());
+            logWriteList.Add(new SqlServerWrite());
+
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                lock (ExceptionStringQueue)
+                {
+                    //从队列中读取日志信息
+                    string str = ExceptionStringQueue.Dequeue();
+                    foreach (var item in logWriteList)
+                    {
+                        item.WriteLogInfo(str);
+                    }
+                }
+            });
+        }
+        public static void WriteLog(string exceptionText)
+        {
+            ExceptionStringQueue.Enqueue(exceptionText);
+        }
+    }
+}
---
+++ ZTB.OA/ZTB.OA.Portal/Controllers/UserInfoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZTB.OA.IBLL;
+
+namespace ZTB.OA.Portal.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        // GET: UserInfo
+       public IUserInfoService UserInfoService { get; set; }
+
+        public ActionResult Index()
+        {
+            ViewData.Model = UserInfoService.GetEntities(t => true).ToList();
+            return View();
+        }
+    }
+}
