43bca58ca0da9375e7a01f172d0b42cdf8cab295
Administrator <Administrator@WIN-JH13BJM99UM>
2016-06-22 15:03:10 +0800

    增加UIDemo

---
+++ ZTB.OA/ZTB.OA.Portal/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZTB.OA.Portal.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
