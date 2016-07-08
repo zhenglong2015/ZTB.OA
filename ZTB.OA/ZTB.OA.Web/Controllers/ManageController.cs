cc7f850a9cd9099dff0e8502aace0bb03a773b8f
Administrator <Administrator@WIN-JH13BJM99UM>
2016-07-08 17:46:53 +0800

    Add 联系我们

--- ZTB.OA/ZTB.OA.Web/Controllers/ManageController.cs
+++ ZTB.OA/ZTB.OA.Web/Controllers/ManageController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZTB.OA.IBLL;
 
 namespace ZTB.OA.Web.Controllers
 {
     public class ManageController : BaseController
     {
+
+        public IUserInfoService UserInfoService { get; set; }
         // 修改头像
         public ActionResult ModifyHead()
         {
@@ -23,6 +26,26 @@ namespace ZTB.OA.Web.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult ModifyPwd(string oldPwd, string newPwd)
+        {
+            if (!string.IsNullOrEmpty(oldPwd) && oldPwd == base.UserInfo.Pwd)
+            {
+                string userId = Request.Cookies["LoginUser"].Value;
+
+                var user = UserInfoService.GetEntities(u => u.Id == UserInfo.Id).FirstOrDefault();
+                user.Pwd = newPwd;
+                UserInfoService.Update(user);
+                Response.Cookies["LoginUser"].Value = null;
+                return Content("ok");
+
+            }
+            else
+            {
+                return Content("原密码输入错误");
+            }
+        }
+
 
         //联系我们
         public ActionResult Contacts()
