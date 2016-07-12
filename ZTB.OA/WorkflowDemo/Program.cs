3445443675b588b61b0007757c707584ae51c92a
zhenglong2015 <853222835@qq.com>
2016-07-12 21:24:47 +0800

    add 工作流Demo

---
+++ ZTB.OA/WorkflowDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+
+namespace WorkflowDemo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity workflow1 = new Activity();
+            WorkflowInvoker.Invoke(workflow1);
+
+
+            Console.ReadKey();
+        }
+    }
+}
