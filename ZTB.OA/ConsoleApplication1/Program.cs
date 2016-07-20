251d928b8636676d7b4c8121c6cdec7144e4c706
Administrator <Administrator@WIN-JH13BJM99UM>
2016-07-20 08:59:46 +0800

    。

--- ZTB.OA/ConsoleApplication1/Program.cs
+++ ZTB.OA/ConsoleApplication1/Program.cs
@@ -14,14 +14,6 @@ namespace ConsoleApplication1
         private static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            //MailMessage myMail = new MailMessage();                //发送端到接收端的邮箱地址  
-            //myMail = new MailMessage("18612081264@163.com", "2850720872@qq.com");
-            //myMail.Subject = "1";
-            ////建立发送对象client,验证邮件服务器，服务器端口，用户名，以及密码  
-            //SmtpClient client = new SmtpClient("123.125.50.133", 25);
-            //client.Credentials = new NetworkCredential("18612081264@163.com", "zl000123");
-            //myMail.Body = "adadjkwedjqiwofhnkjahduiqwfjn";
-            //client.Send(myMail);
             logger.Info("分享快乐");
             Console.ReadKey();
         }
