f053399422c050ec782442c018d18ca458bd6eac
Administrator <Administrator@WIN-JH13BJM99UM>
2016-07-28 15:22:42 +0800

    反射时增加缓存

--- ZTB.OA/ZTB.OA.DALFactory/StaticFactory1.cs
+++ ZTB.OA/ZTB.OA.DALFactory/StaticFactory1.cs
@@ -12,41 +12,67 @@
 namespace ZTB.OA.DALFactory
 {
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using ZTB.OA.EFDAL;
-using ZTB.OA.IDAL;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ZTB.OA.EFDAL;
+    using ZTB.OA.IDAL;
+    using Common.Caches;
 
 
- public partial class StaticFactory
-{   
-	static  string assemblyName = System.Configuration.ConfigurationManager.AppSettings["DalAssemblyName"];
-		
-	public static IActionInfoDal GetActionInfoDal()
-    {          
-        return Assembly.Load(assemblyName).CreateInstance(assemblyName + ".ActionInfoDal") as IActionInfoDal;
-    }
-		
-	public static IRUserActionInfoDal GetRUserActionInfoDal()
-    {          
-        return Assembly.Load(assemblyName).CreateInstance(assemblyName + ".RUserActionInfoDal") as IRUserActionInfoDal;
-    }
-		
-	public static IRoleInfoDal GetRoleInfoDal()
-    {          
-        return Assembly.Load(assemblyName).CreateInstance(assemblyName + ".RoleInfoDal") as IRoleInfoDal;
-    }
-		
-	public static IUserInfoDal GetUserInfoDal()
-    {          
-        return Assembly.Load(assemblyName).CreateInstance(assemblyName + ".UserInfoDal") as IUserInfoDal;
-    }
+    public partial class StaticFactory
+    {
 
-}
+        static string assemblyName = System.Configuration.ConfigurationManager.AppSettings["DalAssemblyName"];
+
+        public static IActionInfoDal GetActionInfoDal()
+        {
+            var tempDal = CacheHelper.GetCache(assemblyName + ".ActionInfoDal");
+            if (tempDal == null)
+            {
+                tempDal = Assembly.Load(assemblyName).CreateInstance(assemblyName + ".ActionInfoDal");
+                CacheHelper.InsertCache(assemblyName + ".ActionInfoDal", tempDal);
+            }
+            return tempDal as IActionInfoDal;
+        }
+
+        public static IRUserActionInfoDal GetRUserActionInfoDal()
+        {
+            var tempDal = CacheHelper.GetCache(assemblyName + ".RUserActionInfoDal");
+            if (tempDal == null)
+            {
+                tempDal = Assembly.Load(assemblyName).CreateInstance(assemblyName + ".RUserActionInfoDal");
+                CacheHelper.InsertCache(assemblyName + ".RUserActionInfoDal", tempDal);
+            }
+            return tempDal as IRUserActionInfoDal;
+        }
+
+        public static IRoleInfoDal GetRoleInfoDal()
+        {
+            var tempDal = CacheHelper.GetCache(assemblyName + ".RoleInfoDal");
+            if (tempDal == null)
+            {
+                tempDal = Assembly.Load(assemblyName).CreateInstance(assemblyName + ".RoleInfoDal");
+                CacheHelper.InsertCache(assemblyName + ".RoleInfoDal", tempDal);
+            }
+            return tempDal as IRoleInfoDal;
+        }
+
+        public static IUserInfoDal GetUserInfoDal()
+        {
+            var tempDal = CacheHelper.GetCache(assemblyName + ".UserInfoDal");
+            if (tempDal == null)
+            {
+                tempDal = Assembly.Load(assemblyName).CreateInstance(assemblyName + ".UserInfoDal");
+                CacheHelper.InsertCache(assemblyName + ".UserInfoDal", tempDal);
+            }
+            return tempDal as IUserInfoDal;
+        }
+
+    }
 
 }
 
