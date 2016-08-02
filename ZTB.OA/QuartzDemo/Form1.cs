a4e0d0d2095144ef86fd17ccf693276caa92a982
Administrator <Administrator@WIN-JH13BJM99UM>
2016-08-02 09:47:41 +0800

    Add QuartzDemo

---
+++ ZTB.OA/QuartzDemo/Form1.cs
@@ -0,0 +1,47 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuartzDemo
+{
+    public partial class Form1 : Form
+    {
+        IScheduler scheduler;
+        public Form1()
+        {
+            InitializeComponent();
+            scheduler = StdSchedulerFactory.GetDefaultScheduler();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (scheduler.IsShutdown)
+                scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            scheduler.Shutdown();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            scheduler.PauseAll();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            scheduler.ResumeAll();
+        }
+    }
+}
---
+++ ZTB.OA/QuartzDemo/TestJob.cs
@@ -0,0 +1,22 @@
+﻿// ==========================================
+// Author                  :  ZTB 
+// Create Time           :    2016/8/1 17:34:47
+// ==========================================
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzDemo
+{
+    public sealed class TestJob : IJob
+    {
+        
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("定时任务1。。。。");
+        }
+    }
+}
